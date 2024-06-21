bc925baf639e2abab3fbb240f4b8a47cfedf4c97
Toshi <jpcmp3@gmail.com>
2024-06-21 22:40:54 +0300

    Added Router and React iconsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import ViewAllJobs from "./components/ViewAllJobs";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={ <h1>My app</h1> } />)
+);
+
+
 const App = () => {
-  return (
-    <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />    
-    </>
-  )
+  return <RouterProvider router={router}/>;
 }
-export default App
+export default App;
