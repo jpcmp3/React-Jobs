64f645952eb5bc1fddc405c152cf9a95c70d78df
Toshi <jpcmp3@gmail.com>
2024-06-22 18:02:14 +0300

    Added NotFountPage, fixed NavBar active classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={ <h1>My app</h1> } />)
-);
+  createRoutesFromElements(
+  <Route path ='/' element={ <MainLayout /> }>
+    <Route index element={ <HomePage /> } />
+    <Route path='/jobs' element={ <JobsPage /> } />
+    <Route path='*' element={ <NotFoundPage /> } />
+  </Route>
+));
 
 
 const App = () => {
